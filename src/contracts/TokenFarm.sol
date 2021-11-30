01072ead8e8b18148e392c3d509b914154135026
chanelle740 <hirwa.chanelle@gmail.com>
2021-11-30 21:54:33 +0200

    solidity extension addeed

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,9 +1,9 @@
-pragma solidity ^0.5.0;
+pragma solidity ^ 0.5 .0;
 
 import "./DappToken.sol";
 import "./DaiToken.sol";
 
-contract TokenFarm{
+contract TokenFarm {
     //All  codes 
     string public name = "Dapp Token Farm";
 
@@ -16,16 +16,17 @@ contract TokenFarm{
     mapping(address => bool) public isStacking;
 
 
-    constructor(DappToken _dappToken, DaiToken _daiToken) public{
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
-        
+
     }
 
-    // Stakes Tokens(Deposit)
+    function stakeTokens(uint _amount) public {
 
-    function stakeTokens(uint _amount) public{
-        
+        //Requiere amount greater than 0
+        require(_amount > 0 , "amount cannot be 0");
+
         //Transfer Mock Dai tokens to this contract for staking.
 
         daiToken.transferFrom(msg.sender, address(this), _amount);
@@ -34,34 +35,34 @@ contract TokenFarm{
 
         stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
 
-      if(!hasStaked[msg.sender]){
-          stakers.push(msg.sender);
-      }
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
 
         // update stacking status
         isStacking[msg.sender] = true;
         hasStaked[msg.sender] = true;
 
-    } 
+    }
 
-      // Issuing Tokens
+    // Issuing Tokens
 
-      function issueTokens() public {
+    function issueTokens() public {
 
-      for(uint i=0; i<stakers.length;i++){
-      address recipient = stakers[i];
-      uint balance = stakingBalance[recipient];
-      if(balance > 0) {
-      dappToken.transfer(recipient, balance);
-      }
-      }
+        for (uint i = 0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if (balance > 0) {
+                dappToken.transfer(recipient, balance);
+            }
+        }
 
-      }
+    }
 
 
     // Unstaking Tokens(Withdraw)
 
-  
+
 
 
 
